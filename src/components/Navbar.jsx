01cfc348769b4b0f7d098403974eabab9fd42377
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-05-17 22:30:55 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="grid grid-cols-12">
+      <div className="col-span-3">
+        <Link className="font-bold" href={"/"}>
+          CareConnect
+        </Link>
+      </div>
+      <div className="col-span-6 flex items-center justify-center gap-3">
+        <Link href={"/"}>Home</Link>
+        <Link href={"/alldoctors"}>Doctors</Link>
+        <Link href={"/desboard"}>Deshboard</Link>
+      </div>
+      <div className="col-span-3 flex items-center justify-center gap-3">
+        <Link href={"/signin"}>Login</Link>
+        <Link href={"/signup"}>Register</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
