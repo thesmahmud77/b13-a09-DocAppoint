a36c041288a617a87627ec833933413855897683
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-05-18 00:04:05 +0600

    Deshboard page Update

--- src/app/deshboard/page.jsx
+++ src/app/deshboard/page.jsx
@@ -1,5 +1,18 @@
+import { Tabs } from "@heroui/react";
+
 const deshboard = () => {
-  return <div>Deshboard</div>;
+  return (
+    <div>
+      <div>
+        <h1 className="text-4xl font-bold">My Bookings</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia,
+          nam fuga vitae ab illum porro
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default deshboard;
