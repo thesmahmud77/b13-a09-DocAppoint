91aee45cba4653cbc5d1f6c566a870c8073a7318
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-05-22 18:25:49 +0600

    Auth process complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 "use client";
+import { signOut, useSession } from "@/lib/auth-client";
+import { Button } from "@heroui/react";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const { data, isPending } = useSession();
+  if (isPending) {
+    return <div>Loding.........</div>;
+  }
+  // console.log(data);
+
+  const user = data?.user;
+
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-3">
@@ -16,8 +26,16 @@ const Navbar = () => {
         <Link href={"/deshboard"}>Deshboard</Link>
       </div>
       <div className="col-span-3 flex items-center justify-center gap-3">
-        <Link href={"/auth/signin"}>Login</Link>
-        <Link href={"/auth/signup"}>Register</Link>
+        {user ? (
+          <Button onClick={() => signOut()} variant="dengar">
+            Logout
+          </Button>
+        ) : (
+          <div>
+            <Link href={"/auth/signin"}>Login</Link>
+            <Link href={"/auth/signup"}>Register</Link>
+          </div>
+        )}
       </div>
     </div>
   );
